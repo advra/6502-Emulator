686a0d210c8fdc6cd8448005d47ed5201bf6a6fb
advra <aalonzo3@gatech.edu>
2020-11-30 20:36:46 -0800

    Added CMake and fixed errors that prevented build

--- src/cpu6502.cpp
+++ src/cpu6502.cpp
@@ -1,9 +1,12 @@
-#pragma
+#include "cpu6502.h"
+#include "bus.h"
 
-#include"cpu6502.h"
+CPU6502::CPU6502(){ }
+
+CPU6502::~CPU6502() { }
 
 uint8_t CPU6502::read(uint64_t addr){
-    bus->read(addr);
+    return bus->read(addr);
 }
 
 void CPU6502::write(uint64_t addr, uint8_t data){
@@ -12,7 +15,15 @@ void CPU6502::write(uint64_t addr, uint8_t data){
 
 void CPU6502::reset(){
     registers.PC = 0xFFFC;
-    registers.SP = 0x0100;
+    registers.SP = 0x00;
     registers.A = registers.X = registers.Y = 0;
-    registers.Status.buf = 0x00;
+    registers.Status.buffer = 0x00;
+}
+
+uint8_t CPU6502::getFlag(FLAG f){
+    return 0x00;
+}
+
+void CPU6502::setFlag(FLAG f, bool v){
+    // registers.Status.buffer
 }
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include "bus.h"
+#include "cpu6502.h"
+
+int main(int argc, char** argv){
+    Bus bus;
+    CPU6502 cpu;
+    return 0;
+}
