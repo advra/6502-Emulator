572278b257df1b8e2e746d35ad71366a21fc28e0
advra <adriandalonzo@gmail.com>
2020-12-10 16:45:34 -0800

    Added instruction look up table

--- include/cpu6502.hpp
+++ include/cpu6502.hpp
@@ -1,10 +1,18 @@
 #pragma once
 
+/*
+    References:
+        official doc        http://archive.6502.org/datasheets/rockwell_r650x_r651x.pdf
+        registers           http://www.obelisk.me.uk/6502/registers.html
+        Addressing Modes    https://wiki.nesdev.com/w/index.php/CPU_addressing_modes http://www.obelisk.me.uk/6502/addressing.html 
+        OpCodes:            http://www.obelisk.me.uk/6502/instructions.html
+        Unimplemnted        http://wiki.nesdev.com/w/index.php/CPU_unofficial_opcodes
+*/
+
 #include <cstdint>
+#include <vector>
 #include "common.hpp"
 
-// Forward declaration of generic communications bus class to
-// prevent circular inclusions
 class Bus;
 
 class CPU6502{
@@ -14,7 +22,6 @@ class CPU6502{
 
         enum FLAG{C,Z,I,D,B,U,V,N};
 
-        // registers http://www.obelisk.me.uk/6502/registers.html
         struct Register{
             u16 PC;                // program counter
             u8 SP;                 // stack pointer
@@ -42,30 +49,65 @@ class CPU6502{
         void write (u64 addr, u8 data);
 
         // Addressing Modes 
-        // https://wiki.nesdev.com/w/index.php/CPU_addressing_modes http://www.obelisk.me.uk/6502/addressing.html 
-        u8 IMP();  u8 IMM();
-        u8 ZP0();  u8 ZPX();
-        u8 ZPY();  u8 REL();
-        u8 ABS();  u8 ABX();
-        u8 ABY();  u8 IND();
-        u8 IZX();  u8 IZY();
+        u8 IMP();   u8 IMM();
+        u8 ZP0();   u8 ZPX();
+        u8 ZPY();   u8 REL();
+        u8 ABS();   u8 ABX();
+        u8 ABY();   u8 IND();
+        u8 IZX();   u8 IZY();
 
-        // opcodes http://www.obelisk.me.uk/6502/instructions.html
-        // LDA();
+        // Load/Store
+        u8 LDA();   u8 LDX();   u8 LDY();  
+        u8 STA();   u8 STX();   u8 STY();   
+        // Register Transfers
+        u8 TAX();   u8 TAY();   u8 TXA();   u8 TYA();   
+        // Stack Operators
+        u8 TSX();   u8 TXS();   u8 PHA();
+        u8 PHP();   u8 PLA();   u8 PLP();
+        // Logical
+        u8 AND();   u8 EOR();   u8 ORA();   u8 BIT();
+        // Arithmetic
+        u8 ADC();   u8 SBC();   u8 CMP();   u8 CPX();   u8 CPY();
+        // Inc/Dec
+        u8 INC();   u8 INX();   u8 INY();
+        u8 DEC();   u8 DEX();   u8 DEY();
+        // Shifts
+        u8 ASL();   u8 LSR();   u8 ROL(); u8 ROR();
+        // Jumps and Calls
+        u8 JMP();   u8 JSR();   u8 RTS();
+        // Branches
+        u8 BCC();   u8 BCS();   u8 BEQ();   u8 BMI();
+        u8 BNE();   u8 BPL();   u8 BVC();   u8 BVS();
+        // Status Flag Changes
+        u8 CLC();   u8 CLD();   u8 CLI();   u8 CLV();   u8 SEC();   u8 SED();   u8 SEI();
+        // Sys Functions
+        u8 BRK();   u8 NOP();   u8 RTI();
+        // Illegal opcode not implemented 
+        u8 XXX(); 
 
-        u8 UOP(); // unofficial opcodes that we do not implement http://wiki.nesdev.com/w/index.php/CPU_unofficial_opcodes
-
-        void clock();
-        // inputs
-        void reset();
-        void irq();         
-        void nmi();       
+        // async input signals
+        // will allow current instructions to complete before interrupt
+        void clock();               
+        void reset();  
+        void irq();             // interrupt request sig 
+        void nmi();             // nonmaskable interrupt sig
 
         u8 fetch();
         u8 fetched = 0x00;
+        
         u16 address_rel = 0x0000;
         u16 address_abs = 0x0000;
         u8 cycles = 0;
+
+        struct INSTRUCTION
+        {
+            std::string name;
+            u8(CPU6502::*operation) (void) = nullptr;
+            u8(CPU6502::*addressingMode) (void) = nullptr;
+            u8 cycles = 0;
+        };
+
+        std::vector<INSTRUCTION> lookup;
 
     private:
         Bus* bus = nullptr;
